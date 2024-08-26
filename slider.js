d78a3760a95ee49ccc674b6f0b1c70a6e8933228
ImamDwiYanPermana <imampermana224@gmail.com>
2024-08-27 05:35:40 +0700

    menambah kan animasi pada h1 di intro dan mengubah ketebalan animasi di batik kayu dan menambahkan gambar di images.

--- slider.js
+++ slider.js
@@ -1,33 +1,13 @@
-let currentIndex = 0;
+document.addEventListener('scroll', function() {
+    const introSection = document.querySelector('.intro-content h1');
+    const introPosition = introSection.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
 
-function showSlide(index) {
-    const carousel = document.querySelector('.carousel');
-    const slides = document.querySelectorAll('.menu-item');
-    const totalSlides = slides.length;
-
-    if (index >= totalSlides) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else {
-        currentIndex = index;
+    if (introPosition < screenPosition) {
+        introSection.style.animation = 'none';
+        introSection.offsetHeight; /* Trigger reflow */
+        introSection.style.animation = 'typing 4s steps(30, end) forwards, blink-caret 0.75s step-end infinite 4s';
     }
-
-    const offset = -currentIndex * 100;
-    carousel.style.transform = `translateX(${offset}%)`;
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
-
-// Add event listener to trigger animation
-document.getElementById('triggerAnimation').addEventListener('click', function() {
-    const animatedText = document.querySelector('.animated-text');
-    animatedText.classList.add('animate');
 });
+
 
